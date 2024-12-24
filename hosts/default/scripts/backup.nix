ce6b159edbc7c331050aa6be3d656db6a9ccea90
YaroslavPanasiuk <yaroslav.panasiuk@lnu.edu.ua>
2024-12-24 23:38:03 +0200

    message

--- hosts/default/scripts/backup.nix
+++ hosts/default/scripts/backup.nix
@@ -10,6 +10,13 @@ cp -rf ~/shared ~/backup
 cp -rf ~/Documents ~/backup
 cp -rf ~/nixos ~/backup
 
+cp -rf ~/.config/dunst ~/nixos/dotfiles
+cp -rf ~/.config/rofi ~/nixos/dotfiles
+cp -rf ~/.config/vlc ~/nixos/dotfiles
+cp -rf ~/.config/wal ~/nixos/dotfiles
+cp -rf ~/.config/walogram ~/nixos/dotfiles
+cp -rf ~/.config/kando ~/nixos/dotfiles
+
 cp -rf ~/backup /run/media/yaros/yaros_usb/yaros_backup
 
 cd ~/nixos
