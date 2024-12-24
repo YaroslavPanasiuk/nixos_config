5671f92678366be872c59c5a53226e1c524e89e6
YaroslavPanasiuk <yaroslav.panasiuk@lnu.edu.ua>
2024-12-24 23:23:13 +0200

    message

--- hosts/default/scripts/backup.nix
+++ hosts/default/scripts/backup.nix
@@ -3,19 +3,15 @@
 pkgs.writeShellScriptBin "backup.sh" '' 
 #!/bin/sh
 
-rm -rf ~/backup
-mkdir -m 777 ~/backup
-cp ~/.bashrc ~/backup
-cp -rf ~/.cache/wal ~/backup
-cp -rf ~/.cache/walogram ~/backup
-cp -rf /etc/nixos ~/backup
+mkdir ~/backup
+cp -rf ~/.cache ~/backup
 cp -rf ~/.config ~/backup
+cp -rf ~/shared ~/backup
 cp -rf ~/Documents ~/backup
-cp -rf ~/shared/Projects/sddm-sugar-dark ~/backup
-cd ~/backup
-git init
+cp -rf ~/nixos ~/backup
+
+cd ~/nixos
 git add .
 git commit -m "message"
-git remote add origin git@github.com:YaroslavPanasiuk/nixos_backup.git
-git push -f origin master
+git push -f origin main
 ''
