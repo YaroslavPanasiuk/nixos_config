6b6eae50007594483bb8408489f8f3096219febd
YaroslavPanasiuk <yaroslav.panasiuk@lnu.edu.ua>
2025-01-15 15:55:37 +0200

    message

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -444,7 +444,7 @@
         "server string" = "smbnix";
         "netbios name" = "smbnix";
         "security" = "user";
-        "hosts allow" = "192.168.31.231 192.168.122.156 127.0.0.1 localhost";
+        "hosts allow" = "192.168.122.75 192.168.31.231 192.168.122.156 127.0.0.1 localhost";
         "hosts deny" = "0.0.0.0/0";
       };
       "shared" = {
